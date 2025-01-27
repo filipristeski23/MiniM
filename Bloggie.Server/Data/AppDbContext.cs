42ea9d9ff6d917b5bbcfb82f5872ba8b1235fc56
Filip <filipristeski23@gmail.com>
2025-01-27 19:37:28 +0100

    Adding the Server folder

---
+++ Bloggie.Server/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Bloggie.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {   
+
+
+        }
+
+
+    }
+}
---
+++ Bloggie.Server/Data/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Bloggie.Data.Entities
+{
+    public class User : IdentityUser
+    {
+
+        public string FullName { get; set; } = string.Empty;
+
+    }
+}
